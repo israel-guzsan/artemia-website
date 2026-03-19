3020f21e4c40f0a9edb8e57a23bebb0b6a176caf
Israel Guzsan <israel.guzsan@gmail.com>
2026-03-19 16:33:07 +0100

    Set site title to 'Artemia' and update docs
    Rename site title in docs/index.html from 'ArtemiaWebsite' to 'Artemia' and update the referenced main bundle to main.8b3e67b13d786587.js. Replace the generated docs main JS (remove main.0ac236945bbda39f.js, add main.8b3e67b13d786587.js). Also includes related source updates to app component templates/styles and src/index.html (likely from a rebuild).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
-  title = 'artemia-website';
+  title = 'Artemia';
 }
