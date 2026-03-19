9a7acc1999bbaab8a7fe3f7eac3655df6065b536
Israel Guzsan <israel.guzsan@gmail.com>
2026-03-19 09:34:32 +0100

    Update docs build artifacts and routes
    Add new generated docs chunk files for several components (personajes, naciones, naturaleza, culturas, najim, facciones, relatos, magia) and include updated main/runtime bundles. Remove old main/runtime artifacts and update docs/index.html to reference the new bundle filenames. Also apply changes to src/app/app.routes.ts (route adjustments). This commit updates the built documentation assets and corresponding HTML to use the new webpack outputs.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,13 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'mundo', loadComponent: () => import('./pages/mundo/mundo.component').then(m => m.MundoComponent) },
   { path: 'historia', loadComponent: () => import('./pages/historia/historia.component').then(m => m.HistoriaComponent) },
+  { path: 'naciones', loadComponent: () => import('./pages/naciones/naciones.component').then(m => m.NacionesComponent) },
+  { path: 'culturas', loadComponent: () => import('./pages/culturas/culturas.component').then(m => m.CulturasComponent) },
+  { path: 'naturaleza', loadComponent: () => import('./pages/naturaleza/naturaleza.component').then(m => m.NaturalezaComponent) },
+  { path: 'magia', loadComponent: () => import('./pages/magia/magia.component').then(m => m.MagiaComponent) },
+  { path: 'najim', loadComponent: () => import('./pages/najim/najim.component').then(m => m.NajimComponent) },
+  { path: 'facciones', loadComponent: () => import('./pages/facciones/facciones.component').then(m => m.FaccionesComponent) },
+  { path: 'personajes', loadComponent: () => import('./pages/personajes/personajes.component').then(m => m.PersonajesComponent) },
+  { path: 'relatos', loadComponent: () => import('./pages/relatos/relatos.component').then(m => m.RelatosComponent) },
   { path: '**', redirectTo: '' }
 ];
